06ede481b1b8a1e80eaf7ac28d9267c38521e65c
Abdullah272056 <abdullah272056@gmail.com>
2021-03-14 13:52:07 +0600

    buttonClick and Intent set

--- app/src/main/java/com/example/blooddonationtest/HomeActivity.java
+++ app/src/main/java/com/example/blooddonationtest/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.blooddonationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -58,6 +59,25 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+    switch (v.getId()){
+    case R.id.logIbButtonID:
+
+        break;
+        case R.id.signUpTextViewId:
+        Intent intent=new Intent(HomeActivity.this,Registration.class);
+        startActivity(intent);
+
+        break;
+        case R.id.forgotPasswordTextViewId:
+
+
+        break;
+
+
+
+
+}
+
 
     }
 }
