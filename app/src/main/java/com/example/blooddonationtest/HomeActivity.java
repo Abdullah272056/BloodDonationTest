72eee65c2869c2ac4c809d621ca1d8f1489612c0
Abdullah272056 <abdullah272056@gmail.com>
2021-03-14 13:47:24 +0600

    view finding and implements onClickListener method

--- app/src/main/java/com/example/blooddonationtest/HomeActivity.java
+++ app/src/main/java/com/example/blooddonationtest/HomeActivity.java
@@ -3,13 +3,18 @@ package com.example.blooddonationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class HomeActivity extends AppCompatActivity {
-    Button button;
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    Button logInButton;
+    TextView signUpTextView,forgotPasswordTextView;
+    EditText logInEmailEditText,logInPasswordEditText;
     // Write a message to the database
     FirebaseDatabase database ;
     DatabaseReference myRef;
@@ -22,6 +27,21 @@ public class HomeActivity extends AppCompatActivity {
         getWindow().setTitle("Homepage");
         setContentView(R.layout.activity_home);
 
+        // view finding
+        logInButton =findViewById(R.id.logIbButtonID);
+        signUpTextView =findViewById(R.id.signUpTextViewId);
+        forgotPasswordTextView =findViewById(R.id.forgotPasswordTextViewId);
+        logInEmailEditText =findViewById(R.id.logInEmailEditTextId);
+        logInPasswordEditText =findViewById(R.id.logInPasswordEditTextId);
+
+        logInButton.setOnClickListener(this);
+        signUpTextView.setOnClickListener(this);
+        forgotPasswordTextView.setOnClickListener(this);
+
+
+
+
+
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -33,6 +53,11 @@ public class HomeActivity extends AppCompatActivity {
 //                Toast.makeText(HomeActivity.this, "Success", Toast.LENGTH_SHORT).show();
 //            }
 //        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
