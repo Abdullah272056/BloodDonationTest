b46e526f83ea46da24060ff0a3ca5f92d7baf1bb
Abdullah272056 <abdullah272056@gmail.com>
2021-03-14 14:14:53 +0600

    create userRegistration  method and input field empty check

--- app/src/main/java/com/example/blooddonationtest/Registration.java
+++ app/src/main/java/com/example/blooddonationtest/Registration.java
@@ -2,7 +2,10 @@ package com.example.blooddonationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,12 +28,71 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
         signUpButton=findViewById(R.id.signUpButtonId);
         signInTextView=findViewById(R.id.signInTextViewId);
 
+        signUpButton.setOnClickListener(this);
+        signInTextView.setOnClickListener(this);
+
 
 
     }
 
     @Override
     public void onClick(View v) {
-        
+        switch (v.getId()){
+            case R.id.signUpButtonId:
+                userRegistration();
+                break;
+
+            case R.id.signInTextViewId:
+
+                Intent intent=new Intent(Registration.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+
+    public  void userRegistration(){
+        final String signInEmail= signUpEmailEditText.getText().toString();
+        final String signInPassword=signUpPasswordEditText.getText().toString();
+        final String signInConfirmPassword=signUpConfirmPasswordEditText.getText().toString();
+
+        if (TextUtils.isEmpty(signInEmail)){
+            signUpEmailEditText.setError("Enter your email");
+            signUpEmailEditText.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(signInEmail).matches()){
+            signUpEmailEditText.setError("Enter a valid  email address");
+            signUpEmailEditText.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(signInPassword)){
+            signUpPasswordEditText.setError("Enter password");
+            signUpPasswordEditText.requestFocus();
+            return;
+        }
+        if (signInPassword.length()<6){
+            signUpPasswordEditText.setError("password should be more than 6");
+            signUpPasswordEditText.requestFocus();
+            return;
+        }
+
+
+        if (TextUtils.isEmpty(signInConfirmPassword)){
+            signUpConfirmPasswordEditText.setError("Enter confirm password");
+            signUpConfirmPasswordEditText.requestFocus();
+            return;
+        }
+        if (signInPassword!=signInConfirmPassword){
+            signUpConfirmPasswordEditText.setError("do not match confirm password");
+            signUpConfirmPasswordEditText.requestFocus();
+            return;
+        }
+
+
+
+
+
     }
 }
