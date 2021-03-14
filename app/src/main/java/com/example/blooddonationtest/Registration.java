c3759f91bffff5072817fd3d25fcbe341342f85f
Abdullah272056 <abdullah272056@gmail.com>
2021-03-14 16:50:19 +0600

    set intent in Registration.java

--- app/src/main/java/com/example/blooddonationtest/Registration.java
+++ app/src/main/java/com/example/blooddonationtest/Registration.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -108,6 +109,10 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
+                            Intent intent=new Intent(Registration.this,LogInActivity.class);
+                            startActivity(intent);
+                            finish();
+                            
                             Toast.makeText(Registration.this, "register success", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(Registration.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
