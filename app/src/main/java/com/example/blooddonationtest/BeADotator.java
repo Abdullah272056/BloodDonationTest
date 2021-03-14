d3a79d0e1b21a31334198099408cf91fd304a5fa
Abdullah272056 <abdullah272056@gmail.com>
2021-03-14 21:30:03 +0600

    create BeADotator Activity

---
+++ app/src/main/java/com/example/blooddonationtest/BeADotator.java
@@ -0,0 +1,14 @@
+package com.example.blooddonationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeADotator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_be_a_dotator);
+    }
+}
