bb9363b26a67d7ace8e8a78c0082fd8537e74744
Abdullah272056 <abdullah272056@gmail.com>
2021-03-14 15:35:31 +0600

    registration function complete

--- app/src/main/java/com/example/blooddonationtest/Registration.java
+++ app/src/main/java/com/example/blooddonationtest/Registration.java
@@ -1,18 +1,27 @@
 package com.example.blooddonationtest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registration extends AppCompatActivity implements View.OnClickListener {
-
+    private FirebaseAuth mAuth;
     EditText signUpEmailEditText, signUpPasswordEditText,signUpConfirmPasswordEditText;
     Button signUpButton;
     TextView signInTextView;
@@ -21,6 +30,10 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         // view finding
          signUpEmailEditText=findViewById(R.id.signUpEmailEditTextId);
         signUpPasswordEditText=findViewById(R.id.signUpPasswordEditTextId);
@@ -30,6 +43,8 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
 
         signUpButton.setOnClickListener(this);
         signInTextView.setOnClickListener(this);
+
+
 
 
 
@@ -84,13 +99,24 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
             signUpConfirmPasswordEditText.requestFocus();
             return;
         }
-        if (signInPassword!=signInConfirmPassword){
+        if (!signInPassword.equals(signInConfirmPassword)){
             signUpConfirmPasswordEditText.setError("do not match confirm password");
             signUpConfirmPasswordEditText.requestFocus();
             return;
         }
 
+        mAuth.createUserWithEmailAndPassword(signInEmail, signInPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(Registration.this, "register success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Registration.this, "register not success", Toast.LENGTH_SHORT).show();
 
+                        }
+                    }
+                });
 
 
 
