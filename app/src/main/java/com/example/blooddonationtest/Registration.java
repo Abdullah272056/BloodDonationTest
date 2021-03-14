c0d9c2da5ea8b78c80095cab99120e5de2c42fcf
Abdullah272056 <abdullah272056@gmail.com>
2021-03-14 13:55:16 +0600

    view finding in Registration

--- app/src/main/java/com/example/blooddonationtest/Registration.java
+++ app/src/main/java/com/example/blooddonationtest/Registration.java
@@ -3,12 +3,28 @@ package com.example.blooddonationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
+
+    EditText signUpEmailEditText, signUpPasswordEditText,signUpConfirmPasswordEditText;
+    Button signUpButton;
+    TextView signInTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        // view finding
+         signUpEmailEditText=findViewById(R.id.signUpEmailEditTextId);
+        signUpPasswordEditText=findViewById(R.id.signUpPasswordEditTextId);
+        signUpConfirmPasswordEditText=findViewById(R.id.signUpConfirmPasswordEditTextId);
+        signUpButton=findViewById(R.id.signUpButtonId);
+        signInTextView=findViewById(R.id.signInTextViewId);
+
+
+
     }
 }
