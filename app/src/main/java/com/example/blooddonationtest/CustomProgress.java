e4b738be41c432cd3f68bb72a2da3f5745dedcf2
Abdullah272056 <abdullah272056@gmail.com>
2021-03-16 19:51:44 +0600

    add code in CustomProgress.java

--- app/src/main/java/com/example/blooddonationtest/CustomProgress.java
+++ app/src/main/java/com/example/blooddonationtest/CustomProgress.java
@@ -7,5 +7,24 @@ import android.view.View;
 import androidx.appcompat.app.AlertDialog;
 
 public class CustomProgress {
-    
+    AlertDialog alertDialog;
+    Context context;
+
+    CustomProgress(Context context) {
+        this.context = context;
+    }
+
+    void showProgress(){
+        AlertDialog.Builder builder     =new AlertDialog.Builder(context);
+        LayoutInflater layoutInflater   =LayoutInflater.from(context);
+        View view                       =layoutInflater.inflate(R.layout.progress_dialog,null);
+        builder.setView(view);
+        alertDialog   = builder.create();
+
+        alertDialog.show();
+    }
+
+    void dismissProgress(){
+        alertDialog.dismiss();
+    }
 }
