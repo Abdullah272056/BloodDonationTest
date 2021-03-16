9d25573f9b031c860dbb696f444a8b53cc544591
Abdullah272056 <abdullah272056@gmail.com>
2021-03-16 11:24:03 +0600

    text view finding

--- app/src/main/java/com/example/blooddonationtest/ProfileActivity.java
+++ app/src/main/java/com/example/blooddonationtest/ProfileActivity.java
@@ -3,12 +3,32 @@ package com.example.blooddonationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView profileFNameTextView,profileNameTextView,profilePhoneTextView,profileEmailTextView,
+            profileBloodGroupTextView,profileAddressTextView,profileLastDonateDateTextView;
+    ImageView profileEditImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // view finding
+        profileFNameTextView=findViewById(R.id.profileFNameTextViewId);
+        profileNameTextView=findViewById(R.id.profileNameTextViewId);
+        profilePhoneTextView=findViewById(R.id.profilePhoneTextViewId);
+        profileEmailTextView=findViewById(R.id.profileEmailTextViewId);
+        profileBloodGroupTextView=findViewById(R.id.profileBloodGroupTextViewId);
+        profileAddressTextView=findViewById(R.id.profileAddressTextViewId);
+        profileLastDonateDateTextView=findViewById(R.id.profileLastDonateDateTextViewId);
+        profileEditImageView=findViewById(R.id.profileEditImageViewId);
+
+
+
+
     }
 }
