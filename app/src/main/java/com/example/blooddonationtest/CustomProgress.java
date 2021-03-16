e84c4ed494743c06fdeebc1aab4f62bf845d5d52
Abdullah272056 <abdullah272056@gmail.com>
2021-03-16 19:51:27 +0600

    create CustomProgress.java

---
+++ app/src/main/java/com/example/blooddonationtest/CustomProgress.java
@@ -0,0 +1,11 @@
+package com.example.blooddonationtest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class CustomProgress {
+    
+}
