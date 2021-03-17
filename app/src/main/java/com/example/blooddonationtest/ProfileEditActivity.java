144a0b9a1ed64fa8836b87d3958658a64bb162e9
Abdullah272056 <abdullah272056@gmail.com>
2021-03-17 19:53:28 +0600

    create selectDate method and call method

--- app/src/main/java/com/example/blooddonationtest/ProfileEditActivity.java
+++ app/src/main/java/com/example/blooddonationtest/ProfileEditActivity.java
@@ -2,17 +2,22 @@ package com.example.blooddonationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class ProfileEditActivity extends AppCompatActivity {
 
     EditText nameEditText,phoneEditText,countryNameEditText,districtEditText,thanaEditText;
     TextView lastDateTextView, bloodGroupTextView;
     Button saveButton;
+    DatePickerDialog.OnDateSetListener mDateSetListener;
 
 
     @Override
@@ -46,9 +51,36 @@ public class ProfileEditActivity extends AppCompatActivity {
         lastDateTextView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-               // selectDate();
+                selectDate();
             }
         });
+
+    }
+
+
+    public void selectDate(){
+
+        Calendar calendar=Calendar.getInstance();
+        int day =calendar.get(Calendar.DAY_OF_MONTH);
+        int month =calendar.get(Calendar.MONTH);
+        int year =calendar.get(Calendar.YEAR);
+
+        // DatePickerDialog create
+        DatePickerDialog dialog=new DatePickerDialog(ProfileEditActivity.this,
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth,mDateSetListener,year,month,day);
+
+        //background color set
+        //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        // selected date set with textView
+        mDateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                lastDateTextView.setText(String.valueOf(dayOfMonth+"/"+month+"/"+year));
+            }
+        };
+
 
     }
 }
