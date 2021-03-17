37470e3280e53e813374a5bf3d753548f4feb1eb
Abdullah272056 <abdullah272056@gmail.com>
2021-03-17 19:43:50 +0600

    add design in activity_profile_edit.xml

--- app/src/main/java/com/example/blooddonationtest/ProfileEditActivity.java
+++ app/src/main/java/com/example/blooddonationtest/ProfileEditActivity.java
@@ -10,5 +10,7 @@ public class ProfileEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_edit);
+
+
     }
 }
