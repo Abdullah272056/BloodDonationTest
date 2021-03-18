004590f17020b6e90a20e1592664fb802d20b496
Abdullah272056 <abdullah272056@gmail.com>
2021-03-18 22:46:13 +0600

    implement Filterable Class and filter data pass

--- app/src/main/java/com/example/blooddonationtest/CustomAdapter.java
+++ app/src/main/java/com/example/blooddonationtest/CustomAdapter.java
@@ -4,21 +4,30 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> implements Filterable {
 
     Context context;
     private List<UserInformation> userInformationList;
 
+    // for search
+    List<UserInformation> copyUserInformationList;
+
     public CustomAdapter(Context context, List<UserInformation> userInformationList) {
         this.context = context;
         this.userInformationList = userInformationList;
+        //for searchView//dataList's copy
+        copyUserInformationList = new ArrayList<>(copyUserInformationList);
+
     }
 
     @NonNull
@@ -43,6 +52,48 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     public int getItemCount() {
         return userInformationList.size();
     }
+
+
+    //call getFilter method
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+    Filter filterData=new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+
+            List<UserInformation> filterList=new ArrayList<>();
+            //for filter data keeping
+            if (charSequence==null||charSequence.length()==0){
+                filterList.addAll(copyUserInformationList);
+            }
+            else{
+                String value=charSequence.toString().toLowerCase().trim();
+                for (UserInformation userInformation:copyUserInformationList){
+                    if (userInformation.getBloodGroup().toLowerCase().trim().contains(value)||userInformation.getDistrictName().toLowerCase().trim().contains(value)){
+                        filterList.add(userInformation);
+                    }
+
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filterList;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            userInformationList.clear();
+            userInformationList.addAll((List)results.values);
+            notifyDataSetChanged();
+        }
+    };
+
+
+
+
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView nameTextView,bloodGroupTextView,locationTextView,lastDateTextView;
