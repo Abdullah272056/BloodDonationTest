d234656e1239f703ecfbd89151382896fe156700
Abdullah272056 <abdullah272056@gmail.com>
2021-03-19 22:58:59 +0600

    create menu and add code

--- app/src/main/java/com/example/blooddonationbd/HomeActivity.java
+++ app/src/main/java/com/example/blooddonationbd/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,6 +14,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -176,6 +178,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
+    
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState){
