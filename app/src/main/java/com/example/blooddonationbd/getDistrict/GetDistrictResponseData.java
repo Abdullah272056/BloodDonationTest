f7cbbf5cd18a6baad8fc691398ba4a62483ec639
Abdullah272056 <abdullah272056@gmail.com>
2021-03-22 09:19:47 +0600

    create GetDistrict package and created model class

---
+++ app/src/main/java/com/example/blooddonationbd/getDistrict/GetDistrictResponseData.java
@@ -0,0 +1,31 @@
+package com.example.blooddonationbd.getDistrict;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class GetDistrictResponseData{
+    @SerializedName("status")
+    @Expose
+    private GetDistrictStatusData getDistrictStatusData;
+    @SerializedName("data")
+    @Expose
+    private List<GetDistrictData> getDistrictData = null;
+
+    public GetDistrictStatusData getGetDistrictStatusData() {
+        return getDistrictStatusData;
+    }
+
+    public void setGetDistrictStatusData(GetDistrictStatusData getDistrictStatusData) {
+        this.getDistrictStatusData = getDistrictStatusData;
+    }
+
+    public List<GetDistrictData> getGetDistrictData() {
+        return getDistrictData;
+    }
+
+    public void setGetDistrictData(List<GetDistrictData> getDistrictData) {
+        this.getDistrictData = getDistrictData;
+    }
+}
---
+++ app/src/main/java/com/example/blooddonationbd/getDistrict/ThanaData.java
@@ -0,0 +1,17 @@
+package com.example.blooddonationbd.getDistrict;
+
+public class ThanaData {
+    String thanaName;
+
+    public ThanaData(String thanaName) {
+        this.thanaName = thanaName;
+    }
+
+    public String getThanaName() {
+        return thanaName;
+    }
+
+    public void setThanaName(String thanaName) {
+        this.thanaName = thanaName;
+    }
+}
