e34db9be3ecc4056068975eb98ffc7ff0b3d6139
Abdullah272056 <abdullah272056@gmail.com>
2021-03-22 09:22:22 +0600

    write code in ApiInterface.java for connect api

--- app/src/main/java/com/example/blooddonationbd/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/blooddonationbd/retrofit/ApiInterface.java
@@ -2,6 +2,9 @@ package com.example.blooddonationbd.retrofit;
 
 
 
+import com.example.blooddonationbd.getDistrict.GetDistrictResponseData;
+import com.example.blooddonationbd.getDivision.GetDivisionResponseData;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -9,15 +12,15 @@ import retrofit2.http.Path;
 public interface ApiInterface {
 
 
-//    // get division name
-//    @GET("api/v1.0/divisions")
-//    Call<GetDivisionResponseData> getAllDivision();
-//
-//
-//    //get single customer information
-//    @GET("api/v1.0/division/{id}")
-//    Call<GetDistrictResponseData> getDistrict(@Path("id") String id);
-//
+    // get division name
+    @GET("api/v1.0/divisions")
+    Call<GetDivisionResponseData> getAllDivision();
+
+
+    //get single customer information
+    @GET("api/v1.0/division/{id}")
+    Call<GetDistrictResponseData> getDistrict(@Path("id") String id);
+
 
 
 }
